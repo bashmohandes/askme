32eebd6ccb021c199758d5ece106032dd8627772
Mohamed Elsherif <bashmohandes@live.com>
2017-08-03 01:06:54 -0700

    Add support for Gulp
    This commit adds support for Gulp and bunch of tasks like
    # nodemon
    # jshint
    # jscs
    # inject

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+'use strict';
+
 var express = require('express');
 
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-app.get('/', function(req, res){
-    res.send('Hello World')
+app.get('/', function (req, res) {
+    res.send('Hello World');
 });
 
-app.listen(port, function(err){
+app.listen(port, function (err) {
     console.log('running server on port', port);
-})
+});
